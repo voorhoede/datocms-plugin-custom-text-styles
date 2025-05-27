e1ec84175882125bfd8965c9b8349f01a5157ced
MarleenEliza <marleen@voorhoede.nl>
2025-05-27 11:16:48 +0200

    restore unique id for inside plugin

--- src/entrypoints/variables.ts
+++ src/entrypoints/variables.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const NODE_OPTIONS: CustomStyleNode[] = [
   { label: "Paragraph", value: "paragraph" },
   { label: "Heading", value: "heading" },
@@ -5,7 +7,7 @@ export const NODE_OPTIONS: CustomStyleNode[] = [
 
 export const DUMMY_CUSTOM_STYLE = () => {
   return {
-    id: 'stand-out',
+    id: uuidv4(),
     title: "Stand Out",
     css: `text-align: center;
 font-size: 24px;
