1e1c9604942a7a598e7a2e87c86f5f56af0eb469
MarleenEliza <marleen@voorhoede.nl>
2025-06-06 10:36:25 +0200

    add info to typing

--- src/main.tsx
+++ src/main.tsx
@@ -19,15 +19,20 @@ connect({
   ) {
     const userParameters = getUserParameters(ctx.plugin.attributes.parameters);
 
-    const customStyles = userParameters.customStyles
-      .flatMap(({ nodes, ...customStyle }) => nodes
-        .map(({ value: node }) => ({
+    /*
+     Data passed on to Structured Text Field
+     For more information, see:
+     https://www.datocms.com/docs/plugin-sdk/structured-text-customizations#adding-custom-styles-to-nodes
+     */
+    const customStyles = userParameters.customStyles.flatMap(
+      ({ nodes, ...customStyle }) =>
+        nodes.map(({ value: node }) => ({
           id: customStyle.slug,
           node,
           label: customStyle.title,
           appliedStyle: getUserStyle(customStyle.css),
         }))
-      );
+    );
 
     return customStyles;
   },
