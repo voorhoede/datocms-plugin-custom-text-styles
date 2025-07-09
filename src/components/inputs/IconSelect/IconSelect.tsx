56897694b8dffdec72410461a4b3a5373f8eca22
MarleenEliza <marleen@voorhoede.nl>
2025-07-09 17:05:27 +0200

    use SelectInput instead of SelectField

--- src/components/inputs/IconSelect/IconSelect.tsx
+++ src/components/inputs/IconSelect/IconSelect.tsx
@@ -1,13 +1,7 @@
-import {
-  SelectField,
-} from "datocms-react-ui";
-import {
-  fas,
-  IconName,
-} from "@fortawesome/free-solid-svg-icons";
+import { SelectInput } from "datocms-react-ui";
+import { fas, IconName } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as styles from "./IconSelect.module.css";
-import { icon } from "@fortawesome/fontawesome-svg-core";
 
 const KEY_NAME = "icon";
 
@@ -17,11 +11,15 @@ const removePrefix = (input: string): string => {
 
 // Ensure a kebab case format since DatoCMS Structured Text Component expects this 'volume-high'.
 const getIconValue = (input: string): string => {
-  return removePrefix(input).replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
-}
+  return removePrefix(input)
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .toLowerCase();
+};
 
 const getIconLabel = (value: string): string => {
-  return removePrefix(value).replace(/([A-Z])/g, " $1").trim();
+  return removePrefix(value)
+    .replace(/([A-Z])/g, " $1")
+    .trim();
 };
 
 type IconSelectProps = {
@@ -35,12 +33,12 @@ type IconSelectProps = {
   onBlur: () => void;
 };
 
-const IconValue = ({ data }: { data: { value: string; label: string } }) => {
+const formatOptionLabel = (data: { label: string; value: string }) => {
   const { label, value } = data;
   return (
-    <div className={styles.iconValue}>
+    <div className={styles.iconOption}>
       <FontAwesomeIcon
-        icon={['fas', value as IconName]}
+        icon={["fas", value as IconName]}
         style={{ marginRight: 8 }}
       />
       <span>{label}</span>
@@ -54,31 +52,6 @@ export const IconSelect = ({
   onChange,
   onBlur,
 }: IconSelectProps) => {
-
-  const IconOption = (props: {
-    data: { value: string; label: string };
-    innerRef: React.RefObject<HTMLDivElement>;
-    innerProps: any;
-    isFocused: boolean;
-    isSelected: boolean;
-  }) => {
-    const { value, label } = props.data as { value: string; label: string, icon: string };
-    return (
-      <div
-        ref={props.innerRef}
-        {...props.innerProps}
-        className={styles.iconOption}
-        data-focused={props.isFocused ? "true" : "false"}
-        data-selected={props.isSelected ? "true" : "false"}>
-        <FontAwesomeIcon
-          icon={['fas', value as IconName]}
-          style={{ marginRight: 8 }}
-        />
-        <span>{label}</span>
-      </div>
-    );
-  };
-
   const options = Object.keys(fas)
     .filter((key) => key !== "prefix") // Remove metadata
     .map((iconKey) => {
@@ -90,24 +63,16 @@ export const IconSelect = ({
       };
     });
   return (
-    <SelectField
+    <SelectInput
       id={`${KEY_NAME}-${index}`}
       name={KEY_NAME}
-      label='Icon'
-      placeholder='e.g. "volume-high"'
+      formatOptionLabel={formatOptionLabel}
       value={selected}
       onChange={(newValue) => {
         onChange(index, KEY_NAME, newValue as CustomMark[keyof CustomMark]);
       }}
-      selectInputProps={{
-        isMulti: false,
-        options,
-        onBlur,
-        components: {
-          Option: IconOption as any,
-          SingleValue: IconValue,
-        },
-      }}
+      onBlur={onBlur}
+      options={options}
     />
   );
 };
